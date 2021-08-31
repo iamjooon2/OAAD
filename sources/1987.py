75fa3df5ffdcc70779dda4f7b63c8e7a92b9a1c2
junheeyeap <junheeyeap@gmail.com>
2021-08-31 23:43:20 +0900

    solved 1942

--- sources/1987.py
+++ sources/1987.py
@@ -7,8 +7,8 @@ for i in nums:
     if (i==1):
         continue
     for j in range(2,i+1):
-        if(i%j==0):
+        if (i%j==0):
             count+=1
-    if(count==1):
+    if (count==1):
         prime_num+=1
 print(prime_num)
