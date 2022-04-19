b1541672a9b12c20b79957928dc34415f0930232
iamjooon2 <iamjooon2@gmail.com>
2022-04-20 00:04:27 +0900

    change into pythonic codes

--- baekjoon/2309.py
+++ baekjoon/2309.py
@@ -1,20 +1,18 @@
 heights = []
-
 tmp1, tmp2 = 0,0
 
-for _ in range(9):
-    a = int(input())
-    heights.append(a)
+for i in range(9):
+    heights.append(int(input()))
 
 for i in range(9):
     for j in range(i+1, 9):
         if sum(heights) - (heights[i]+heights[j]) == 100:
-            tmp1 = heights[i]
-            tmp2 = heights[j]
+            tmp1, tmp2 = heights[i], heights[j]
 
 heights.remove(tmp1)
 heights.remove(tmp2)
 
 heights.sort()
+
 for n in range(7):
     print(heights[n])
