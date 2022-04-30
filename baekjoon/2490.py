29e2ddaa96c4f5ce1e82b6fbf559f08e78892d01
iamjooon2 <iamjooon2@gmail.com>
2022-04-30 17:24:17 +0900

    change structure

---
+++ baekjoon/2490.py
@@ -0,0 +1,8 @@
+for i in range(3):
+    scores = list(map(int, input().split()))
+    count = scores.count(0)
+    if count == 1: print('A')
+    elif count == 2: print('B')
+    elif count == 3: print('C')
+    elif count == 4: print('D')
+    else: print('E')
