0c09d33612f8b1282e86ccad05f06d321020ad0c
iamjooon2 <iamjooon2@gmail.com>
2022-08-11 15:45:06 +0900

    이코테: 탐색 - DFS/BFS

---
+++ lecture/dongbinbook/ch5_search/BFS.py
@@ -0,0 +1,38 @@
+# BFS - 너비 우선 탐색
+# 큐를 사용하며 동작하고, 큐 자려구조를 이용하여 구현한다
+# #deque는 양방향 입출력이 간으한 자료구조이지만, queue 라이브러리보다 편리하므로 사용함. 그래서 한 방향으로만 뺄라고 popleft() 사용 - 원래 양쪽 다 가능한놈이니까
+
+from collections import deque
+
+# BFS 메서드 정의
+def bfs(graph, start, visited):
+    # 큐 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = deque.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+bfs(graph, 1, visited)
