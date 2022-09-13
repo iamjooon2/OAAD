0f98644ecf2a25c3408cc41caeb98916af182447
iamjooon2 <iamjooon2@gmail.com>
2022-09-13 22:15:48 +0900

    이코테 Python Algorithm: sort

---
+++ lecture/dongbinbook/ch6_sort/bubble_sort.py
@@ -0,0 +1,10 @@
+array = [3,5,2,11,4,7,13]
+
+for i in range(len(array)-1, 0, -1):
+    # i번 반복
+    for j in range(i):
+        # j번째 요소가 j+1번째 요소보다 더 크다면 위치 변경
+        if array[j] > array[j + 1]:
+            array[j], array[j + 1] = array[j + 1], array[j]
+
+print(array)
--- lecture/dongbinbook/ch6_sort/insertion_sort.py
+++ lecture/dongbinbook/ch6_sort/insertion_sort.py
@@ -1,11 +1,14 @@
 array = [7,5,9,0,3,1,6,2,4,8]
 
 for i in range(len(array)):
-    for j in range(i, 0, -1): #인덱스 i부터 1까지 감소하며 반복
+    # 인덱스 i부터 1까지 감소하며 반복
+    for j in range(i, 0, -1):
+        # 한 칸씩 왼쪽으로 이동하며
         if array[j] < array[j-1]:
-            array[j], array[j-1] = array[j-1], array[j] #swap
+            # 위치 변경
+            array[j], array[j-1] = array[j-1], array[j]
+        # 자기 자신보다 작은 원소를 만나면 그 위치에서 멈춘다
         else:
             break
 
 print(array)
-#range(start, end, step)
