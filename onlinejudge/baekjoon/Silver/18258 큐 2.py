536d8537c06cca00c1cd1a5f649866be34499255
iamjooon2 <iamjooon2@gmail.com>
2022-12-27 02:09:03 +0900

    BOJ: 18258 큐 2

--- onlinejudge/baekjoon/Silver/18258 큐 2.py
+++ onlinejudge/baekjoon/Silver/18258 큐 2.py
@@ -1,33 +1,3 @@
-def push(num):
-    list.append(num)
-
-def pop():
-    if len(list) != 0:
-        print(list.popleft())
-    else:
-        print(-1)
-
-def size():
-    print(len(list))
-
-def empty():
-    if len(list) != 0:
-        print(0)
-    else:
-        print(1)
-
-def front():
-    if len(list) != 0:
-        print(list[0])
-    else:
-        print(-1)
-
-def back():
-    if len(list) != 0:
-        print(list[-1])
-    else:
-        print(-1)
-
 import sys
 # 큐 구현을 위한 deque
 from collections import deque
@@ -35,15 +5,28 @@ from collections import deque
 list = deque([])
 for i in range(int(sys.stdin.readline())):
     x = sys.stdin.readline().split()
-    if x[0] == 'push':
-        push(x[1])
-    elif x[0] =='pop':
-        pop()
-    elif x[0] == 'size':
-        size()
-    elif x[0] == 'empty':
-        empty()
-    elif x[0] == 'front':
-        front()
+    num = x[0]
+    if num == 'push':
+        list.append(num)
+    elif num == 'pop':
+        if len(list) != 0:
+            print(list.popleft())
+        else:
+            print(-1)
+    elif num == 'size':
+        print(len(list))
+    elif num == 'empty':
+        if len(list) != 0:
+            print(0)
+        else:
+            print(1)
+    elif num == 'front':
+        if len(list) != 0:
+            print(list[0])
+        else:
+            print(-1)
     else:
-        back()
+        if len(list) != 0:
+            print(list[-1])
+        else:
+            print(-1)
