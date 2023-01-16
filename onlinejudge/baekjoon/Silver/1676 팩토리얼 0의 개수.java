4709ac2ad60f503dd5479b715a8aec341593e824
iamjooon2 <iamjooon2@gmail.com>
2023-01-17 00:22:39 +0900

    BOJ: 1676 팩토리얼 0의 개수

---
+++ onlinejudge/baekjoon/Silver/1676 팩토리얼 0의 개수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int count = 0;
+
+        while (num >= 5) {
+            count += num / 5;
+            num /= 5;
+        }
+        System.out.println(count);
+    }
+}
