3ca4434452ff2b9735854e6349e0f1b4ff89c8c1
iamjooon2 <iamjooon2@gmail.com>
2023-01-31 22:42:44 +0900

    BOJ: 9012 괄호

--- onlinejudge/baekjoon/Silver/9012 괄호.java
+++ onlinejudge/baekjoon/Silver/9012 괄호.java
@@ -1,46 +1,50 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 class Main {
+    private static final String YES = "YES";
+    private static final String NO = "NO";
+    private static final char INIT_BRACKET = '(';
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int n = scan.nextInt();
         scan.nextLine();
 
-        ArrayList<String> list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            list.add(scan.nextLine());
+        ArrayList<String> lineList = new ArrayList<>();
+        for (int i = 0; i < n; i += 1) {
+            lineList.add(scan.nextLine());
         }
 
-        for (int i = 0; i < n; i++) {
-            Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < n; i += 1) {
+            Stack<Character> stringStack = new Stack<>();
 
             boolean flag = false;
-            String s = list.get(i);
+            String s = lineList.get(i);
 
-            for (int j = 0; j < s.length(); j++) {
+            for (int j = 0; j < s.length(); j += 1) {
                 char c = s.charAt(j);
 
-                if (c == '(') {
-                    stack.push(c);
+                if (c == INIT_BRACKET) {
+                    stringStack.push(c);
                 } else {
-                    if (!stack.isEmpty()) {
-                        stack.pop();
+                    if (!stringStack.isEmpty()) {
+                        stringStack.pop();
                     } else {
                         flag = true;
                         break;
                     }
                 }
             }
-            if (flag || !stack.isEmpty()) {
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
-            }
+            printAnswer(stringStack, flag);
         }
     }
-    
+
+    private static void printAnswer(Stack<Character> stack, boolean flag) {
+        if (flag || !stack.isEmpty()) {
+            System.out.println(NO);
+        } else {
+            System.out.println(YES);
+        }
+    }
+
 }
