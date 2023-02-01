5c2fc4900eea240f732a29f98df582d8f531a981
iamjooon2 <iamjooon2@gmail.com>
2023-02-01 20:04:31 +0900

    BOJ: 10828 스택

--- onlinejudge/programmerss/level_1/서울에서 김서방 찾기.py
+++
@@ -1,4 +0,0 @@
-def solution(seoul):
-    for i in range(len(seoul)):
-        if seoul[i] == "Kim":
-            return "김서방은 " + str(i) + "에 있다"
--- onlinejudge/programmerss/level_1/음양 더하기.py
+++
@@ -1,8 +0,0 @@
-def solution(absolutes, signs):
-    answer = 0
-    for i in range(len(signs)):
-        if signs[i] == False:
-            absolutes[i] = absolutes[i] * -1
-    for i in range(len(absolutes)):
-        answer += absolutes[i]
-    return answer
