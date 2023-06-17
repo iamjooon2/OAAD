66e7b1c34523b1b55d39ea3fb5fa894cb7aad134
iamjooon2 <iamjooon2@gmail.com>
2023-06-18 01:41:01 +0900

    programmers: 문자열 다루기 기본

---
+++ onlinejudge/programmers/level_1/문자열 다루기 기본.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if (s.length() != 4 && s.length() != 6)  {
+            answer = false;
+        }
+        
+        for (char c : s.toCharArray()) {
+            if (c < 48 || c > 57) {
+                answer = false;
+            }
+        }
+        return answer;
+    }
+}
