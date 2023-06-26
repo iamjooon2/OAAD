4148bc5264447aecbb165624f1843c0e1defa918
iamjooon2 <iamjooon2@gmail.com>
2023-06-27 00:10:29 +0900

    programmers: 3진법 뒤집기

---
+++ onlinejudge/programmers/level_1/3진법 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        String answer = "";
+
+        while(n > 0) {
+            answer = (n % 3) + answer;
+            n /= 3;
+        }
+        answer = new StringBuilder(answer).reverse().toString();
+
+        return Integer.parseInt(answer, 3);
+    }
+}
