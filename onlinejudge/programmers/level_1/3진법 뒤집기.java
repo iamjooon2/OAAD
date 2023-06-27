17944f8dceaab7119d2c993843448c50cc55614c
iamjooon2 <iamjooon2@gmail.com>
2023-06-28 00:43:15 +0900

    x만큼 간격이 있는 n개의 숫자

--- onlinejudge/programmers/level_1/3진법 뒤집기.java
+++ onlinejudge/programmers/level_1/3진법 뒤집기.java
@@ -6,8 +6,9 @@ class Solution {
             answer = (n % 3) + answer;
             n /= 3;
         }
+
         answer = new StringBuilder(answer).reverse().toString();
 
-        return Integer.parseInt(answer, 3);
+        return Integer.parseInt(answer, 3); // 3진법으로 변환
     }
 }
