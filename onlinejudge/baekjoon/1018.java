153c65e33db29fffc7c0a887f8a854ce81ca5ffc
iamjooon2 <iamjooon2@gmail.com>
2024-01-02 15:27:20 +0900

    boj 1018 체스판 다시 칠하기

---
+++ onlinejudge/baekjoon/1018.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Solution{
+
+    static char[][] BLACK_START = {
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+    };
+    static char[][] WHITE_START = {
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+    };
+
+    static int N, M;
+    static char[][] board;
+    static int WHITES = Integer.MAX_VALUE;
+    static int BLACKS = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        initialize();
+        findInSurround();
+        System.out.println(Math.min(WHITES, BLACKS));
+    }
+
+    private static void findInSurround() {
+        for (int i = 0; i <= M - 8; i++) {
+            for (int j = 0; j <= N - 8; j++) {
+                find(i, j);
+            }
+        }
+    }
+
+    private static void find(int height, int width) {
+        int startWithWhiteCount = 0;
+        int startWithBlackCount = 0;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i + width][j + height] != WHITE_START[i][j]) {
+                    startWithWhiteCount++;
+                }
+                if (board[i + width][j + height] != BLACK_START[i][j]) {
+                    startWithBlackCount++;
+                }
+            }
+        }
+
+        WHITES = Math.min(startWithWhiteCount, WHITES);
+        BLACKS = Math.min(startWithBlackCount, BLACKS);
+    }
+
+    private static void initialize() {
+        Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+        M = scan.nextInt();
+
+        board = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            char[] line = scan.next().toCharArray();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = line[j];
+            }
+        }
+    }
+}
