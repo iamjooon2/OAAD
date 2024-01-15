b58c0c9f5f93acf8062ac4c5b9cf53891a1f2677
iamjooon2 <iamjooon2@gmail.com>
2024-01-15 16:03:14 +0900

    leetcode: 125  Valild Palindrome

---
+++ onlinejudge/leetcode/125-Valid Palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static boolean isPalindrome(String s) {
+        String sentence = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        for (int i = 0; i < sentence.length() / 2; i += 1) {
+            if (sentence.charAt(i) != sentence.charAt(sentence.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
