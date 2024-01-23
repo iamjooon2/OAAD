6df5e9a483a38b22606f4e89cf5201a80e586a6c
iamjooon2 <iamjooon2@gmail.com>
2024-01-23 15:07:45 +0900

    leecode: 509 Fibonacci Number(dp)

--- onlinejudge/leetcode/509-Fibonacci Number.java
+++ onlinejudge/leetcode/509-Fibonacci Number.java
@@ -1,4 +1,5 @@
 class Solution {
+    // recursion
     public int fib(int n) {
         if (n == 0) {
             return 0;
@@ -9,5 +10,23 @@ class Solution {
 
         return fib(n - 1) + fib(n - 2);
     
+    }
+}
+
+class Solution {
+    // dp
+    public int fib(int n) {
+        if (n == 1) return 1;
+        if (n == 0) return 0;
+        
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i <= n; i += 1) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        
+        return arr[n];
     }
 }
