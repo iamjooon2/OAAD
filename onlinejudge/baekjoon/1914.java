e388c7b753daa14d1b67877499cc87b998fa28a2
iamjooon2 <iamjooon2@gmail.com>
2024-01-23 14:53:44 +0900

    leecode: 509 Fibonacci Number

--- onlinejudge/baekjoon/1914.java
+++ onlinejudge/baekjoon/1914.java
@@ -9,30 +9,27 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         N = scan.nextInt();
 
-        // 1개 전처리
-        if (N == 1) {
-            System.out.println(1);
-            System.out.println("1 3");
-            return;
-        }
-
-        // 20개 미만일때 이동 출력
+        // 20개 이하일 때 이동 출력
         if (N <= 20) {
             printMoves();
             recursive(BigInteger.valueOf(N), 1, 3, 2);
             return;
         }
 
+        // 20개 초과 바로 결과 출력
         printMoves();
     }
 
     private static void printMoves() {
         // 2^n - 1 개의 움직임만 있으면 됨
-        BigInteger value = BigInteger.valueOf(2).pow(N).subtract(BigInteger.ONE);
+        BigInteger value = BigInteger.valueOf(2)
+                .pow(N)
+                .subtract(BigInteger.ONE);
         System.out.println(value);
     }
 
     private static void recursive(BigInteger total, int source, int target, int temp) {
+        // 개수가 1개일 때 종료
         if (total.equals(BigInteger.ONE)) {
             System.out.println(source + " " + target);
             return;
