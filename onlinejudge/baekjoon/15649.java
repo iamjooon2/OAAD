0cdb904c29431229b61a3c8b4773f7b4ee141f8a
iamjooon2 <iamjooon2@gmail.com>
2024-02-15 19:25:34 +0900

    boj 15649: N과 M(1)

---
+++ onlinejudge/baekjoon/15649.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        visited = new boolean[n];
+
+        backTracking(0);
+        System.out.println(sb);
+    }
+
+    static void backTracking(int depth) {
+        // 만약 depth 가 m이면 return
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            arr[depth] = i + 1;
+            backTracking(depth + 1);
+            visited[i] = false;
+        }
+
+    }
+
+}
