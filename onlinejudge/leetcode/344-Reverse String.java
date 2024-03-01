363770ea06c6dcc5ec550f2e24e95d3c6510e9b4
iamjooon2 <iamjooon2@gmail.com>
2024-03-02 03:48:39 +0900

    leetcode : reverse string

--- onlinejudge/leetcode/344-Reverse String.java
+++ onlinejudge/leetcode/344-Reverse String.java
@@ -1,13 +1,9 @@
 class Solution {
     public void reverseString(char[] s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length; i++) {
-            stack.push(s[i]);
-        }
-        
-        for (int i = 0; i < s.length; i++) {
-            s[i] = stack.pop();
+        for (int i = 0; i < s.length / 2; i += 1) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
         }
     }
 }
