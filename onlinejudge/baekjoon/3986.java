2983e8221287741241f62d2e793d420d7bda7235
iamjooon2 <iamjooon2@gmail.com>
2024-05-13 10:09:37 +0900

    boj 3986: 좋은 단어

---
+++ onlinejudge/baekjoon/3986.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    static int N;
+    static int count = 0;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            String word = scan.next();
+
+            Stack<Character> stack = new Stack<>();
+            for (char alphabet : word.toCharArray()) {
+                if (stack.isEmpty()) {
+                    stack.push(alphabet);
+                    continue;
+                }
+
+                if (stack.peek().equals(alphabet)) {
+                    stack.pop();
+                } else {
+                    stack.push(alphabet);
+                }
+            }
+
+            if (stack.isEmpty()) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
