c7bb7667f67ac9450b07fbdd33441b1dcf3bf498
iamjooon2 <iamjooon2@gmail.com>
2024-11-17 23:39:27 +0900

    leetcode 151: 손코딩 추가

--- onlinejudge/leetcode/151-Reverse Words in a String.java
+++ onlinejudge/leetcode/151-Reverse Words in a String.java
@@ -13,3 +13,38 @@ public class Solution {
         return result.toString();
     }
 }
+
+
+// 손ㅗ딩
+public class Solution {
+    public String reverseWords(String s) {
+        List<Character> result = new ArrayList<>();
+        char[] arr = s.trim().toCharArray();
+
+        boolean wasBlank = false;
+        for (int i = 0; i < arr.length; i += 1) {
+            boolean isBlank = ' ' == arr[i];
+            
+            if (wasBlank && isBlank) {
+                continue;
+            }
+
+            result.add(arr[i]);
+
+            wasBlank = isBlank;
+        }
+
+        String sentence = "";
+        for (int i = 0; i < result.size(); i += 1) {
+            sentence += String.valueOf(result.get(i));
+        }
+
+        List<String> answer = Arrays.stream(sentence.split(" "))
+            .collect(Collectors.toList());
+
+        Collections.reverse(answer);
+
+        return answer.stream()
+            .collect(Collectors.joining(" "));
+    }
+}
