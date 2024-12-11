207bad165b7d84ff6e381d020d11dd5c8f459cb8
iamjooon2 <iamjooon2@gmail.com>
2024-12-11 19:37:14 +0900

    leetcode 380

---
+++ onlinejudge/leetcode/380.java
@@ -0,0 +1,49 @@
+class RandomizedSet {
+
+    final Map<Integer, Integer> map;
+    int randomIndex;
+
+    public RandomizedSet() {
+        map = new HashMap<>();
+        randomIndex = 0;
+    }
+    
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        map.put(val, 0);
+        
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (map.containsKey(val)) {
+            map.remove(val);
+            
+            return true;
+        }
+
+        return false;
+    }
+    
+    public int getRandom() {
+        int size = map.size();
+        
+        List<Integer> keys = map.keySet()
+            .stream()
+            .collect(Collectors.toList());
+
+        Collections.shuffle(keys);
+
+        return keys.get(0);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
