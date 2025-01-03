e5c42bff71dd80cc3a4353009f210ac1ecb0a1f5
iamjooon2 <iamjooon2@gmail.com>
2025-01-03 18:38:59 +0900

    leetcode 2770 - 반복 한번만 하도록

--- onlinejudge/leetcode/2770.java
+++ onlinejudge/leetcode/2770.java
@@ -1,12 +1,32 @@
+// class Solution {
+//     public int waysToSplitArray(int[] nums) {
+//         int n = nums.length;
+
+//         long[] beforeSum = new long[n];
+//         beforeSum[0] = (long) nums[0];
+//         for (int i = 1; i < n; i += 1) {
+//             beforeSum[i] = beforeSum[i - 1] + (long) nums[i];
+//         }
+
+//         long[] afterSum = new long[n];
+//         afterSum[n - 1] = (long) nums[n - 1];
+//         for (int i = n - 2; i >= 0; i -= 1) {
+//             afterSum[i] = afterSum[i + 1] + (long) nums[i];
+//         }
+
+//         int answer = 0;
+//         for (int i = 0; i < n - 1; i += 1) {
+//             if (beforeSum[i] >= afterSum[i + 1]) {
+//                 answer += 1;
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int waysToSplitArray(int[] nums) {
         int n = nums.length;
-
-        long[] beforeSum = new long[n];
-        beforeSum[0] = (long) nums[0];
-        for (int i = 1; i < n; i += 1) {
-            beforeSum[i] = beforeSum[i - 1] + (long) nums[i];
-        }
 
         long[] afterSum = new long[n];
         afterSum[n - 1] = (long) nums[n - 1];
@@ -14,8 +34,12 @@ class Solution {
             afterSum[i] = afterSum[i + 1] + (long) nums[i];
         }
 
+        long[] beforeSum = new long[n];
         int answer = 0;
+        beforeSum[0] = (long) nums[0];
         for (int i = 0; i < n - 1; i += 1) {
+            beforeSum[i + 1] = beforeSum[i] + (long) nums[i + 1];
+
             if (beforeSum[i] >= afterSum[i + 1]) {
                 answer += 1;
             }
