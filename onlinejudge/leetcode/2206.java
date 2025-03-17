4443c57159d33b1b075e34e0f56275b89000ac4a
iamjooon2 <iamjooon2@gmail.com>
2025-03-17 16:35:08 +0900

    leetcode 2206

---
+++ onlinejudge/leetcode/2206.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+
+        return set.isEmpty();
+    }
+}
